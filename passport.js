161f73379f3e07cb6e55829bfaac2cfaaca7fd59
ETweb51 <webdev.et@gmail.com>
2021-09-04 12:21:27 +0200

    Task 2.9 fixed

--- passport.js
+++ passport.js
@@ -1,26 +1,28 @@
 const passport = require('passport'),
-LocalStrategy = require('passport-local').Strategy,
-Models = require('./models.js'),
-passportJWT = require('passport-jwt');
+  LocalStrategy = require('passport-local').Strategy,
+  Models = require('./models.js'),
+  passportJWT = require('passport-jwt');
 
 let Users = Models.User,
-JWTStrategy = passportJWT.Strategy,
-ExtractJWT = passportJWT.ExtractJwt;
+  JWTStrategy = passportJWT.Strategy,
+  ExtractJWT = passportJWT.ExtractJwt;
 
 passport.use(new LocalStrategy({
-  nameField: 'Name',
+  usernameField: 'Username',
   passwordField: 'Password'
-}, (name, password, callback) => {
-  console.log(name + ' ' + password);
-  Users.findOne({Name: name}, (error, user) => {
-    if(error) {
+}, (username, password, callback) => {
+  console.log(username + '  ' + password);
+  Users.findOne({ Name: username }, (error, user) => {
+    if (error) {
       console.log(error);
       return callback(error);
     }
+
     if (!user) {
       console.log('incorrect username');
       return callback(null, false, {message: 'Incorrect username or password.'});
     }
+
     console.log('finished');
     return callback(null, user);
   });
@@ -30,11 +32,11 @@ passport.use(new JWTStrategy({
   jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
   secretOrKey: 'your_jwt_secret'
 }, (jwtPayload, callback) => {
-  return Users.findbyId(jwtPayload._id)
-  .then((user) => {
-    return callback(null, user);
-  })
-  .catch ((error) => {
-    return callback(error);
-  });
+  return Users.findById(jwtPayload._id)
+    .then((user) => {
+      return callback(null, user);
+    })
+    .catch((error) => {
+      return callback(error)
+    });
 }));
